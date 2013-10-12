e53c61867fcc4e051d41d2a31d9c77a4a1579731
jaivalis <johnaivalis@gmail.com>
2013-10-12 16:04:28 +0200

    Added main for experiment 2 (minimax Q-learning)

---
+++ src/a3/Experiment2.java
@@ -0,0 +1,11 @@
+package a3;
+
+/**
+ * In this experiment we will implement the minimax Q-Learning algorithm and apply it to a 1v1 scenario.
+ */
+public class Experiment2 {
+
+    public void main(String[] args) {
+
+    }
+}
--- src/episode/EpisodeGenerator.java
+++ src/episode/EpisodeGenerator.java
@@ -8,7 +8,6 @@ import state.State;
 import java.util.ArrayList;
 
 public class EpisodeGenerator {
-
     private ArrayList<Predator> predators;
     private Prey prey;
 
@@ -34,6 +33,7 @@ public class EpisodeGenerator {
         State s_prime = initialState;
 
         int steps = 0;
+        System.out.println("step #"+steps + " " + s);
         do {
         	steps++;
             JointAction ja =  new JointAction(s, this.prey, this.predators);
@@ -45,6 +45,7 @@ public class EpisodeGenerator {
             double preyReward = s_prime.getPreyReward();
             double predReward = s_prime.getPredatorReward();
             episode.addStep(s, ja, preyReward, predReward, s_prime);
+            System.out.println("step #"+steps + " " + s);
         } while(!s_prime.isTerminal() && steps < 1000000);
 
         episode.refreshDiscounted(gamma);
