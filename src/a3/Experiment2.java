a3416286aa120213af058760a3f74a0c6988f4a9
Francesco Stablum <stablum@gmail.com>
2013-10-18 23:01:33 +0200

    from John: Experiment2.java

--- src/a3/Experiment2.java
+++ src/a3/Experiment2.java
@@ -1,68 +1,42 @@
-package a3;
+	
 
-import policy.QDeterministicPolicy;
-import agent.Agent;
-import agent.AgentsCollection;
-import algorithms.QLearning;
-import episode.EpisodeGenerator;
-import state.ReducedState;
-import util.Util;
-
-/**
- * In this experiment we will implement independent Q-Learning algorithm and apply it to a multi-agent system.
- */
-public class Experiment2 {
-
-    public static void main(String[] args) {
-//        int predatorCount = 2;
-        System.out.println("predatorCount, episodeCount, averageEpisodeSize, preyWins, predatorsWin");
-
-        for (int predatorCount = 1; predatorCount < 5; predatorCount++) {
-            for(int episodeCount = 50; episodeCount < Util.EPISODE_COUNT; episodeCount += 50) {
-                // 1. generate environment
-                AgentsCollection agents = Builder.experiment2Config(predatorCount, Util.INITIAL_Q_VALUE);
-                ReducedState simulationInitialState = new ReducedState(agents.getPredatorsCoordinates());
-
-                // 2. train
-                QLearning.run(agents, Util.ALPHA, Util.GAMMA, episodeCount);
-                System.out.println("%trained.");
-
-                // 3. simulate & output results
-                Util.PREY_WINS = 0;
-                Util.PREDATORS_WIN = 0;
-                EpisodeGenerator simulator = new EpisodeGenerator(agents);
-                double averageEpisodeSize = simulator.getAverageEpisodeSize(simulationInitialState, Util.NUMBER_OF_TEST_RUNS);
-//                System.out.println(episodeCount + ", " + averageEpisodeSize);
-                System.out.println(predatorCount+", "+episodeCount+", "+averageEpisodeSize+", "+Util.PREY_WINS+", "+Util.PREDATORS_WIN);
-
-
-        System.out.println("episodeCount, averageRounds");
-        for(int episodeCount = 50; episodeCount < Util.EPISODE_COUNT; episodeCount += 50) {
-
-        	AgentsCollection agents = Builder.experiment2Config(predatorCount, Util.INITIAL_Q_VALUE);
-
-        	// 1. train
-            QLearning.run(agents, Util.ALPHA, Util.GAMMA, episodeCount);
-            System.out.println("%trained.");
-            
-            // after the Q has been learned, 
-            // we apply a Q-based deterministic policy to the agents
-            for(Agent agent : agents) {
-            	QDeterministicPolicy pi = new QDeterministicPolicy();
-            	pi.setQ(agent.q);
-            	agent.setPolicy(pi);
-            }
-            
-            EpisodeGenerator simulator = new EpisodeGenerator(agents);
-            ReducedState simulationInitialState = new ReducedState(agents.getPredatorsCoordinates());
-
-            // 2. simulate & output results
-            double averageRounds = 0.0; //algos.getSimulationAverageRounds(simulations);
-            for (int i = 0; i < Util.NUMBER_OF_TEST_RUNS; i++) {
-                Episode episode = simulator.generate(simulationInitialState, Util.GAMMA);
-                averageRounds += episode.size();
-                System.out.println(episode.size());
+    package a3;
+     
+    import agent.AgentsCollection;
+    import algorithms.QLearning;
+    import episode.EpisodeGenerator;
+    import state.ReducedState;
+    import util.Util;
+     
+    /**
+     * In this experiment we will implement independent Q-Learning algorithm and apply it to a multi-agent system.
+     */
+    public class Experiment2 {
+     
+        public static void main(String[] args) {
+    //        int predatorCount = 2;
+            System.out.println("predatorCount, episodeCount, averageEpisodeSize, preyWins, predatorsWin");
+     
+            for (int predatorCount = 1; predatorCount < 5; predatorCount++) {
+                for(int episodeCount = 50; episodeCount < Util.EPISODE_COUNT; episodeCount += 50) {
+                    // 1. generate environment
+                    AgentsCollection agents = Builder.experiment2Config(predatorCount, Util.INITIAL_Q_VALUE);
+                    ReducedState simulationInitialState = new ReducedState(agents.getPredatorsCoordinates());
+     
+                    // 2. train
+                    QLearning.run(agents, Util.ALPHA, Util.GAMMA, episodeCount);
+                    System.out.println("%trained.");
+     
+                    // 3. simulate & output results
+                    Util.PREY_WINS = 0;
+                    Util.PREDATORS_WIN = 0;
+                    EpisodeGenerator simulator = new EpisodeGenerator(agents);
+                    double averageEpisodeSize = simulator.getAverageEpisodeSize(simulationInitialState, Util.NUMBER_OF_TEST_RUNS);
+    //                System.out.println(episodeCount + ", " + averageEpisodeSize);
+                    System.out.println(predatorCount+", "+episodeCount+", "+averageEpisodeSize+", "+Util.PREY_WINS+", "+Util.PREDATORS_WIN);
+     
+                }
             }
         }
     }
-}
+
